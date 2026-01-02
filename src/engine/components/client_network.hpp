d484f73d2188d9d72370eb204c84fee383034401
Quinn Carmack <quinncarmack@gmail.com>
2026-01-02 18:16:07 -0500

    working on networking rehaul

--- src/engine/components/client_network.hpp
+++ src/engine/components/client_network.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <enet.h>
 #include "messages.pb.h"
+#include "spdlog/spdlog.h"
 
 class ClientNetwork {
     friend class ClientEngine;
@@ -46,7 +47,7 @@ public:
         return nullptr;
     };
 
-    template<typename T> void send(const T &msg, bool flush = false) {
+    template<typename T> void send(const T &input, bool flush = false) {
         static_assert(std::is_base_of_v<ClientMsg, T>, "T must be a subclass of ClientMsg");
 
         ENetPacketFlag flag = ENET_PACKET_FLAG_RELIABLE;
@@ -55,38 +56,35 @@ public:
             flag = ENET_PACKET_FLAG_UNRELIABLE_FRAGMENT;
         }
 
-        ENetPacket* packet;
-        bz::ServerMsg msg;
+        bz::ClientMsg msg;
 
-        if (constexpr (std::is_same_v<T, ClientMsg_Init)) {
-            msg.mutable_init_msg();
-            msg->set_name(msg->name);
-        } else if (constexpr (std::is_same_v<T, ClientMsg_Chat)) {
-            msg.mutable_chat_msg();
-            msg->set_toid(msg->toId);
-            msg->set_text(msg->text);
-        } else if (constexpr (std::is_same_v<T, ClientMsg_Location)) {
-            msg.mutable_location_msg();
-            msg->mutable_location_msg()->mutable_position()->set_x(msg->position.x);
-            msg.mutable_location_msg()->mutable_position()->set_y(msg->position.y);
-            msg.mutable_location_msg()->mutable_position()->set_z(msg->position.z);
-            msg.mutable_location_msg()->mutable_rotation()->set_w(msg->rotation.w);
-            msg.mutable_location_msg()->mutable_rotation()->set_x(msg->rotation.x);
-            msg.mutable_location_msg()->mutable_rotation()->set_y(msg->rotation.y);
-            msg.mutable_location_msg()->mutable_rotation()->set_z(msg->rotation.z);
-        } else if (constexpr (std::is_same_v<T, ClientMsg_RequestSpawn)) {
-            msg.mutable_request_spawn_msg();
-        } else if (constexpr (std::is_same_v<T, ClientMsg_CreateShot>)) {
-            msg.mutable_create_shot_msg();
-            msg->mutable_create_shot_msg()->set_localshotid(msg->localShotId);
-            msg->mutable_create_shot_msg()->mutable_position()->set_x(msg->position.x);
-            msg->mutable_create_shot_msg()->mutable_position()->set_y(msg->position.y);
-            msg->mutable_create_shot_msg()->mutable_position()->set_z(msg->position.z);
-            msg->mutable_create_shot_msg()->mutable_velocity()->set_x(msg->velocity.x);
-            msg->mutable_create_shot_msg()->mutable_velocity()->set_y(msg->velocity.y);
-            msg->mutable_create_shot_msg()->mutable_velocity()->set_z(msg->velocity.z);
+        if constexpr (std::is_same_v<T, ClientMsg_Init>) {
+            msg.mutable_init()->set_name(input.name);
+        } else if constexpr (std::is_same_v<T, ClientMsg_Chat>) {
+            msg.mutable_chat()->set_toid(input.toId);
+            msg.mutable_chat()->set_text(input.text);
+        } else if constexpr (std::is_same_v<T, ClientMsg_Location>) {
+            auto* loc = msg.mutable_location();
+            loc->mutable_position()->set_x(input.position.x);
+            loc->mutable_position()->set_y(input.position.y);
+            loc->mutable_position()->set_z(input.position.z);
+            loc->mutable_rotation()->set_w(input.rotation.w);
+            loc->mutable_rotation()->set_x(input.rotation.x);
+            loc->mutable_rotation()->set_y(input.rotation.y);
+            loc->mutable_rotation()->set_z(input.rotation.z);
+        } else if constexpr (std::is_same_v<T, ClientMsg_RequestSpawn>) {
+            msg.mutable_request_spawn();
+        } else if constexpr (std::is_same_v<T, ClientMsg_CreateShot>) {
+            auto* shot = msg.mutable_create_shot();
+            shot->set_localshotid(input.localShotId);
+            shot->mutable_position()->set_x(input.position.x);
+            shot->mutable_position()->set_y(input.position.y);
+            shot->mutable_position()->set_z(input.position.z);
+            shot->mutable_velocity()->set_x(input.velocity.x);
+            shot->mutable_velocity()->set_y(input.velocity.y);
+            shot->mutable_velocity()->set_z(input.velocity.z);
         } else {
-            spdlog::error("ServerNetwork::send: Unsupported message type");
+            spdlog::error("ClientNetwork::send: Unsupported message type");
             return;
         }
         
